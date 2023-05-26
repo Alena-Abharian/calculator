e860566d22cd9b04d6c1bc946942d2d37513b403
Alena Abharian <alenar2531@gmail.com>
2023-05-25 23:15:10 -0700

    calculator finished

--- calculator.js
+++ calculator.js
@@ -1,3 +1,21 @@
+const inputA = document.querySelector('.input_a');
+const inputB = document.querySelector('.input_b');
+const selectOperation = document.querySelector('.operation');
+const btnResult = document.querySelector('.btn_result');
+const output = document.querySelector('.output');
+
+btnResult.addEventListener('click', function (){
+    const a= Number(inputA.value);
+    const b = Number(inputB.value);
+    const operation = selectOperation.value;
+
+    const result = calculate({a, b, operation});
+
+    output.textContent = result
+    console.log(result)
+
+})
+
 const OPERATIONS = {
     summation : "+",
     subtraction: "-",
